2dc0188fa9eff0c9c5dc448c3c623c891b0a4258
Govind Karki <govind050796@gmail.com>
2023-04-23 22:59:20 +0530

    node updates and throttle emit

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,8 @@
 import { config } from '@/common/config'
 import { constants } from '@/common/constants'
-import { throttleFilter, useMouse, useStorage } from '@vueuse/core'
+import { throttleFilter, useMouse, useStorage, useThrottleFn } from '@vueuse/core'
 import io from 'socket.io-client'
-import { reactive, ref, unref, watch } from 'vue'
+import { reactive, ref, unref, watch, type Ref } from 'vue'
 
 interface Props {
   roomId: string
@@ -10,10 +10,21 @@ interface Props {
   onUserJoined: Function
   onUserLeft: Function
   onUserUpdate: Function
+  applyEdgeChanges: Function
+  applyNodeChanges: Function
+  vueflow: Ref
 }
 
 export const useSocketIO = (options: Props) => {
-  const { roomId, onInit, onUserJoined, onUserLeft, onUserUpdate } = options
+  const {
+    roomId,
+    onInit,
+    onUserJoined,
+    onUserLeft,
+    onUserUpdate,
+    applyEdgeChanges,
+    applyNodeChanges
+  } = options
   const name = unref(useStorage(constants.storageKeys.USERNAME, '', sessionStorage))
   const socket = ref(io(config.socketUrl, { query: { name, roomId } }))
 
@@ -22,6 +33,7 @@ export const useSocketIO = (options: Props) => {
   watch(mousePos, () => {
     socket.value.emit(constants.socketEvents.MOUSE_POS_UPDATE, mousePos)
   })
+
   socket.value.on(constants.socketEvents.USER_LIST, (data) => {
     onInit(data)
   })
@@ -34,9 +46,19 @@ export const useSocketIO = (options: Props) => {
   socket.value.on(constants.socketEvents.USER_UPDATE, (data) => {
     onUserUpdate(data)
   })
+  socket.value.on(constants.socketEvents.NODE_CHANGE, (data) => {
+    applyNodeChanges(data)
+  })
+  socket.value.on(constants.socketEvents.EDGE_CHANGE, (data) => {
+    applyEdgeChanges(data)
+  })
+
+  const throttledEmit = useThrottleFn((event, data) => {
+    socket.value.emit(event, data)
+  }, 10)
 
   return {
-    send: socket.value.emit,
+    send: throttledEmit,
     close: socket.value.disconnect,
     socket
   }
